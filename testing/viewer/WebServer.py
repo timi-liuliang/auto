0eb2a3742db46bec52a68061a24bec7b08d32c44
Captain <qq79402005@gmail.com>
2021-08-07 22:52:15 +0800

    show all figures with flask (web server) #5

--- testing/viewer/WebServer.py
+++ testing/viewer/WebServer.py
@@ -1,10 +1,17 @@
-from flask import Flask
+import os
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def figure_main_viwer():
-    return "<p>Hello World</p>"
+    figure_content = "<!DOCTYPE html>"
+
+    for file in os.listdir(os.getcwd() + "/static/images/"):
+        if file.endswith(".gif"):
+            figure_content = figure_content + "<p><img src=\"/static/images/{0}\" ></p>".format(file)
+
+    return figure_content
 
 if __name__ == '__main__':
     app.run(debug=True)
